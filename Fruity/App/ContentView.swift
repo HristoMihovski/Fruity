1c8de6b330033b29b39fc9ba29333caeb95f88bb
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-09 22:09:13 +0200

    Commit1

--- Fruity/App/ContentView.swift
+++ Fruity/App/ContentView.swift
@@ -8,17 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var fruits: [Fruit] = fruitsData
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(fruits) { item in
+                    FruitRowView(fruit: item)
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(fruits: fruitsData)
 }
--- Fruity/View/FruitRowView.swift
+++ Fruity/View/FruitRowView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct FruitRowView: View {
+    
+    var fruit: Fruit
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: Color(red : 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
+                .background(Image(fruit.gradientColor))
+                .cornerRadius(8)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundStyle(Color.secondary)
+            }
+        }
     }
-}
-
-#Preview {
-    FruitRowView()
+    
+    struct FruitRowView_Previews: PreviewProvider {
+        static var previews: some View {
+            FruitRowView(fruit: fruitsData[0])
+                .previewLayout(.sizeThatFits)
+                .padding()
+        }
+    }
 }
