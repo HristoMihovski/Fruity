ebfb15c698b8ed2fede131cadc95465c10cb2698
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-09 22:08:35 +0200

    Commit

---
+++ Fruity/View/FruitRowView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitRowView.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 9.11.23.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitRowView()
+}
