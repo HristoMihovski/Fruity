669b192217656754d04a9a842bc50be9cded751b
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-12 10:46:20 +0200

    Commit

---
+++ Fruity/App/ContentViewWinter.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewWinter.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 12.11.23.
+//
+
+import SwiftUI
+
+struct ContentViewWinter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewWinter()
+}
