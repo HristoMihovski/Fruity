f59d7459f42bc79ec3a2500cc120f09cb2d9d540
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-21 15:02:57 +0200

    Made some changes with the project settings. Made some UX and UI design changes.

---
+++ Fruity/View/FruitHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitHeaderView.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 20.11.23.
+//
+
+import SwiftUI
+
+struct FruitHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitHeaderView()
+}
---
+++ Fruity/View/SourceLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  SourceLinkView.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 20.11.23.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SourceLinkView()
+}
