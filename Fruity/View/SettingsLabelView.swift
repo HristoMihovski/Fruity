1610f9406331b27cfc55173029e6c504272abf80
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-22 21:34:23 +0200

    MAde a new "Settings" option from where you can see some info for the app and the developer and you can restart the app to the OnboardingScreen

---
+++ Fruity/View/SettingsLabelView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsLabelView.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 22.11.23.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsLabelView()
+}
---
+++ Fruity/View/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 22.11.23.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
