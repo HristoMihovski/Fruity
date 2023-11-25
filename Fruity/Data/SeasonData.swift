fc1f01c073925380ecd4c5146b9563fea55e569e
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-25 20:41:34 +0200

    Upgraded the side menu

---
+++ Fruity/Data/SeasonData.swift
@@ -0,0 +1,27 @@
+//
+//  SeasonData.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 25.11.23.
+//
+
+import SwiftUI
+
+let seasonData: [Season] = [
+    Season(
+    title: "Пролет",
+    image: "cloud.sun.fill"
+    ),
+    Season(
+    title: "Лято",
+    image: "sun.max.fill"
+    ),
+    Season(
+    title: "Есен",
+    image: "leaf.fill"
+    ),
+    Season(
+    title: "Зима",
+    image: "cloud.snow.fill"
+    )
+]
--- Fruity/View/SideMenuView.swift
+++ Fruity/View/SideMenuView.swift
@@ -10,61 +10,69 @@ import SwiftUI
 struct SideMenuView: View {
   
     var fruits: [Fruit] = fruitsData
+    var seasons: [Season] = seasonData
     @AppStorage("isContent") var isContent: Bool?
     
   var body: some View {
-    VStack {
-      Text("Choose a season of your choice:")
-            .fontWeight(.bold)
-        .font(.title)
-        .foregroundColor(.black)
-        .multilineTextAlignment(.center)
-      
-      Divider()
-        .frame(width: 300, height: 2)
-        .background(Color.black)
-        .padding(.horizontal, 16)
-      
-      // MARK: - Content
-      
-        NavigationLink(destination: ContentView2()) {
-            Text("Spring")
-            Image(systemName: "cloud.sun")
-                .padding(.vertical, 4)
+      NavigationView {
+          VStack {
+          Text("Choose a season of your choice:")
+                .fontWeight(.bold)
+            .font(.title)
+            .foregroundColor(.black)
+            .multilineTextAlignment(.center)
+          
+          Divider()
+            .frame(width: 300, height: 2)
+            .background(Color.black)
+            .padding(.horizontal, 16)
+          
+          // MARK: - Content
+          
+            
+                    ForEach(seasons[0...0]) { item in
+                        NavigationLink(destination: ContentView2()) {
+                            SeasonRowView(seasons: item)
+                                .padding(.vertical, 4)
+                        }
+                    }
+                
+                .font(.title)
+              ForEach(seasons[1...1]) { item in
+                  NavigationLink(destination: ContentView2()) {
+                      SeasonRowView(seasons: item)
+                          .padding(.vertical, 4)
+                  }
+              }
+          
+          .font(.title)
+              ForEach(seasons[2...2]) { item in
+                  NavigationLink(destination: ContentView()) {
+                      SeasonRowView(seasons: item)
+                          .padding(.vertical, 4)
+                  }
+              }
+          
+          .font(.title)
+              ForEach(seasons[3...3]) { item in
+                  NavigationLink(destination: ContentView1()) {
+                      SeasonRowView(seasons: item)
+                          .padding(.vertical, 4)
+                  }
+              }
+          
+          .font(.title)
+          
+          Spacer()
         }
-      .font(.title)
-      .foregroundColor(.black)
-        NavigationLink(destination: ContentView()) {
-            Text("Summer")
-            Image(systemName: "sun.max")
-                .padding(.vertical, 4)
-        }
-      .font(.title)
-      .foregroundColor(.black)
-        NavigationLink(destination: ContentView()) {
-            Text("Autumn")
-            Image(systemName: "leaf")
-                .padding(.vertical, 4)
-        }
-      .font(.title)
-      .foregroundColor(.black)
-        NavigationLink(destination: ContentView1()) {
-            Text("Winter")
-            Image(systemName: "cloud.snow")
-                .padding(.vertical, 4)
-        }
-      .font(.title)
-      .foregroundColor(.black)
-      
-      Spacer()
-    }
-    .padding(32)
-    .background(Color.white)
+        .padding(32)
+        .background(Color.white)
     .edgesIgnoringSafeArea(.bottom)
+      }
   }
-  
+
 }
 
 #Preview {
-    SideMenuView(fruits: fruitsData)
+    SideMenuView(fruits: fruitsData, seasons: seasonData)
 }
