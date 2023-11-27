998b175838248e6a830e968a99af0a8fd5a34111
HristoMihovski <hristo.mihovski.highschool@svetlina.net>
2023-11-27 19:50:51 +0200

    1

---
+++ Fruity/App/ContentView3.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView3.swift
+//  Fruity
+//
+//  Created by Violeta.Valcheva on 27.11.23.
+//
+
+import SwiftUI
+
+struct ContentView3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView3()
+}
